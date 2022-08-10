54bf3fc7ef8961dfb86773ec1587192d6a5ca4af
meltyyyyy <takeru.abe.g1@gmail.com>
2022-08-10 22:24:29 +0900

    update _tfidf.py

--- scripts/linear/svc/_tfidf.py
+++ scripts/linear/svc/_tfidf.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python
 # coding: utf-8
-"""summery
-fold0 : 0.580672820967852
-fold1 : 0.6412732828174005
-fold2 : 0.6598340782164313
-fold3 : 0.6116684415438964
-fold4 : 0.651368611047707
-oof score: 0.6289634469186574
-"""
 
 from cProfile import label
 from sklearn.metrics import f1_score
